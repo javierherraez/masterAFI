3d0392d720ba70b5808c9ad74650a6aed2d2e2ed
Javier Herarez <javier.herraez.jh@gmail.com>
2022-03-08 20:58:28 +0100

    0803

--- 15. Aprendizaje supervisado II/02. Random forest y otros ensembles/ejercicios/lab1_randomForest/randomforest.py
+++ 15. Aprendizaje supervisado II/02. Random forest y otros ensembles/ejercicios/lab1_randomForest/randomforest.py
@@ -5,7 +5,12 @@ Created on Sun Jan 31 15:47:28 2016
 @author: Álvaro Barbero Jiménez
 """        
 
+from cmath import sqrt
 from sklearn.base import ClassifierMixin
+from sklearn.tree import DecisionTreeClassifier
+
+import numpy as np
+import random
             
 class RandomForest(ClassifierMixin):
     """Custom implementation of the Random Forest model for classification
@@ -23,13 +28,25 @@ class RandomForest(ClassifierMixin):
         
     def fit(self, X, y):
         """Fits the Random Forest to a given training dataset"""
-        pass  # TODO
+        for i in range(self.nestimators):
+            Xtrain, ytrain = bootstrap(X, y)
+            decisiontree = DecisionTreeClassifier(max_features = 'sqrt')
+            self._estimators.append(decisiontree.fit(Xtrain, ytrain))
+        return self
     
     def predict(self, X):
         """Generates predictions for a given unlabeled dataset"""
-        pass  # TODO
-
+        prediction_out = []
+        predictions = [tree.predict(X) for tree in self._estimators]
+        for i in range(len(predictions[0])):
+            aux_list = [prediction[i] for prediction in predictions]
+            prediction_out.append(most_common(aux_list))
+        return np.array(prediction_out)
 
 def bootstrap(X, y):
     """Returns a bootstrap sample of the given labeled dataset"""
-    pass  # TODO
+    index = random.choices(range(len(X)), k = len(X))
+    return (X[index], y[index])
+
+def most_common(lst):
+    return max(set(lst), key=lst.count)
